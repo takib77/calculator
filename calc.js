a489060a90a5cd72cf70429a86a3cbad18d8b2d5
Leo <takib77@gmail.com>
2020-11-28 17:59:03 +0100

    Click event

--- calc.js
+++ calc.js
@@ -1,28 +1,32 @@
 'use strict'
 
-//Mező és gombok kiválasztása
+// Mező és gombok kiválasztása
 
-const output = (document.querySelector('#input[data-field]'));
+const clickItems = document.querySelectorAll('.buttons[data-btn]')
+const output = document.querySelector('#input[data-field]');
 const buttons = Array
     .from(document.querySelectorAll('.buttons[data-btn]'))
     .map(item => item.textContent);
-;
 
 
-//console.log(output);
-console.log(buttons);
+// Esemény létrehozása minden elemre
 
 
-const handleClick = () => {
-    for (let i = 0; i < buttons.length; i += 1) {
-        return console.log(buttons[i].textContent);
-    }
+const buttonEvent = () => {
+    clickItems.forEach(item => {
+        item.addEventListener('click', handleClick);
+    })
 };
 
 
-//
-//
-//const opSum = buttons[0].addEventListener('click', handleClick);
-//
-//console.log(opSum);
+// Esemény folyamata
+
+const handleClick = () => {
+    console.log(buttons);
+};
+
+
+// Esemény lefuttatása
+
+buttonEvent()
 
