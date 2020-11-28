5fd75ef82f5e81a46b3c7e04a7899e6f2f2433b0
Leo <takib77@gmail.com>
2020-11-28 20:59:00 +0100

    JS trying buttons values

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@
 
 // Mező és gombok kiválasztása
 
-const clickItems = document.querySelectorAll('.buttons[data-btn]')
+const clickItems = document.querySelectorAll('.buttons[data-btn]');
 const output = document.querySelector('#input[data-field]');
 const buttons = Array
     .from(document.querySelectorAll('.buttons[data-btn]'))
@@ -22,11 +22,40 @@ const buttonEvent = () => {
 // Esemény folyamata
 
 const handleClick = () => {
-    console.log(buttons);
+    for (let i = 0; i < buttons.length; i += 1) {
+        output.value += buttons[i];
+    }
+    if (output.value.length > 22) {
+        output.value = 'error';
+    }
 };
-
 
 // Esemény lefuttatása
 
 buttonEvent()
 
+
+
+
+
+
+
+
+
+
+//const sum = buttons[0];
+//const sub = buttons[1];
+//const multi = buttons[2];
+//const divis = buttons[3];
+//const seven = buttons[4];
+//const eight = buttons[5];
+//const nine = buttons[6];
+//const four = buttons[7];
+//const five = buttons[8];
+//const six = buttons[9];
+//const one = buttons[10];
+//const two = buttons[11];
+//const three = buttons[12];
+//const zero = buttons[13];
+//const point = buttons[14];
+//const equal = buttons[15];
