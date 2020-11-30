4eb7ce8b67a7bcb87deecd623013213a1afcb7b5
Leo <takib77@gmail.com>
2020-11-30 14:40:08 +0100

    Buttons works

--- calc.js
+++ calc.js
@@ -1,38 +1,34 @@
 'use strict'
 
-// Mező és gombok kiválasztása
 
-const clickItems = document.querySelectorAll('.buttons[data-btn]');
-const output = document.querySelector('#input[data-field]');
-const buttons = Array
-    .from(document.querySelectorAll('.buttons[data-btn]'))
-    .map(item => item.textContent);
+const writeOutput = (value) => {
+    document.querySelector('#input').value += value
+}
 
-
-// Esemény létrehozása minden elemre
-
-
-const buttonEvent = () => {
+const handleCalcClick = () => {
+    const clickItems = document.querySelectorAll('#calc');
     clickItems.forEach(item => {
-        item.addEventListener('click', handleClick);
+        item.addEventListener('click', (event) => {
+            writeOutput(event.target.dataset.btn)
+        })
+    })
+};
+
+const cancelClick = () => {
+    const delOutput = document.querySelector('.cancel');
+    delOutput.addEventListener('click', (event) => {
+        document.querySelector('#input').value = '';
+    })
+};
+
+const equalClick = () => {
+    const getResultInOutput = document.querySelector('.equal');
+    getResultInOutput.addEventListener('click', (event) => {
+        writeOutput(event.target.dataset.equal)
     })
 };
 
 
-// Esemény folyamata
-
-const handleClick = () => {
-    for (let i = 0; i < buttons.length; i += 1) {
-        output.value += buttons[i];
-    }
-    if (output.value.length > 22) {
-        output.value = 'error';
-    }
-};
-
-// Esemény lefuttatása
-
-buttonEvent()
 
 
 
@@ -43,19 +39,11 @@ buttonEvent()
 
 
 
-//const sum = buttons[0];
-//const sub = buttons[1];
-//const multi = buttons[2];
-//const divis = buttons[3];
-//const seven = buttons[4];
-//const eight = buttons[5];
-//const nine = buttons[6];
-//const four = buttons[7];
-//const five = buttons[8];
-//const six = buttons[9];
-//const one = buttons[10];
-//const two = buttons[11];
-//const three = buttons[12];
-//const zero = buttons[13];
-//const point = buttons[14];
-//const equal = buttons[15];
+
+
+
+
+
+handleCalcClick();
+cancelClick();
+equalClick();
