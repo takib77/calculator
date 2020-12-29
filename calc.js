633776f2ccce02e0261e91097e0c553f4317a84a
Leo <takib77@gmail.com>
2020-12-29 18:28:12 +0100

    Working with 2 numbers

--- calc.js
+++ calc.js
@@ -1,20 +1,41 @@
-'use strict'
+'use strict';
+
+
+// Gombok, kijelző
+
+const output = document.querySelector('#input');
+const clickItems = document.querySelectorAll('#calc');
+const delOutput = document.querySelector('.cancel');
+const getResultInOutput = document.querySelector('.equal');
+
+
+// Elvégezhető műveletek listája, működésük és egyéb változók
+
+const sum = (x, y) => (x + y);
+const sub = (x, y) => (x - y);
+const mul = (x, y) => (x * y);
+const div = (x, y) => (x / y);
+const signes = ['+', '-', '×', '÷'];
+let numbers = [];
+let operators = [];
+let calcform = [];
+let calc = [];
+let result = 0;
 
 
 // Gombleütések megjelentése a kijelzőn
 
 const writeOutput = (value) => {
-    document.querySelector('#input').value += value
-}
+    output.value += value
+};
 
 
 // Gombleütéseket figyelő esemény (kivéve C és =)
 
 const handleCalcClick = () => {
-    const clickItems = document.querySelectorAll('#calc');
     clickItems.forEach(item => {
         item.addEventListener('click', (event) => {
-            writeOutput(event.target.dataset.btn)
+            writeOutput(event.target.textContent)
         })
     })
 };
@@ -23,51 +44,37 @@ const handleCalcClick = () => {
 // Gombleütés figyelő - C és kijelzőmező nullázása
 
 const cancelClick = () => {
-    const delOutput = document.querySelector('.cancel');
     delOutput.addEventListener('click', (event) => {
-        document.querySelector('#input').value = '';
+        output.value = '';
+        calc = [];
+        numbers = [];
+        operators = [];
+        calcform = [];
+        result = 0;
     })
-    calc = [];
-    result = 0;
 };
 
 
 // Gombleütés figyelő - = és majd eredmény megjelenítése
 
 const equalClick = () => {
-    const getResultInOutput = document.querySelector('.equal');
     getResultInOutput.addEventListener('click', (event) => {
-        document.querySelector('#input').value = '';
-        writeOutput(event.target.dataset.equal);
-        writeOutput(result);
+        numsAndOperatorsSeparator();
+        calculator(calcform);
+        if (numbers.includes(NaN)) {
+            output.value = 'ERROR!';
+        } else {
+            output.value = result;
+        }
     })
 };
 
 
-// Eseménye eltávolító
-
-const removeListenerCancel = () => {
-    delOutput.removeEventListener('click', cancelClick)
-};
-
-
-// Elvégezhető műveletek listája és működésük
-
-const sum = (x, y) => (x + y);
-const sub = (x, y) => (x - y);
-const mul = (x, y) => (x * y);
-const div = (x, y) => (x / y);
-const signes = ['+', '-', '×', '÷'];
-
-
 // Bevitt karakterek rendezése
 
-const numbers = [];
-const operators = [];
-const calcform = [];
+let num = '';
 const numsAndOperatorsSeparator = () => {
-    const outputString = document.querySelector('#input').value;
-    let num = '';
+    const outputString = output.value;
     for (let i = 0; i < outputString.length; i += 1) {
         if (signes.indexOf(outputString[i]) === -1) {
             num += outputString[i];
@@ -81,41 +88,40 @@ const numsAndOperatorsSeparator = () => {
     }
     numbers.push(parseFloat(num));
     calcform.push(parseFloat(num));
-    if (numbers.includes(NaN)) {
-        document.querySelector('#input').value = 'ERROR!';
-    }
+    num = '';
     console.log('Számok:', numbers, 'Jelek:', operators);
-}
+};
 
 
 // Műveletek végrehajtása
 
-let calc = [];
-let result = 0;
 const calculator = (arr) => {
-    calc.push(calcform[0])
+    if (calcform.length === 0) {
+        calc.push(0);
+    } else {
+        calc.push(calcform[0]);
+    }
     for (let i = 0; i < arr.length; i += 1) {
         calc.push(arr[i + 1], arr[i + 2]);
         console.log('PUSH', calc);
         if (arr[i + 1] === '+') {
-            result = sum(calc[0], calc[2]);
+            return result = sum(calc[0], calc[2]);
         } else if (arr[i + 1] === '-') {
-            result = sub(calc[0], calc[2]);
+            return result = sub(calc[0], calc[2]);
         } else if (arr[i + 1] === '×') {
-            result = mul(calc[0], calc[2]);
+            return result = mul(calc[0], calc[2]);
         } else if (arr[i + 1] === '÷') {
-            result = div(calc[0], calc[2]);
+            return result = div(calc[0], calc[2]);
         }
         calc = [result];
         console.log(result);
-        calcform.splice(0, 2, [result]);
+        calcform.slice(0, 1, result);
     }
+    return result;
 };
 
 
 
 handleCalcClick();
-numsAndOperatorsSeparator();
-calculator(calcform);
-//cancelClick();
+cancelClick();
 equalClick();
